226bf410b85ff403ff1250468f8d80a26c33fc2f
Carlos <ninjacar04@gmail.com>
2026-01-13 10:19:46 +0100

    Añadido la ruta de estadisticas

--- src/router/Index.tsx
+++ src/router/Index.tsx
@@ -3,9 +3,11 @@ import { createBrowserRouter } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
 import NewVideo from "../pages/NewVideo";
+import Stats from "../pages/Stats";
 
 export const router = createBrowserRouter([
     {path: "/", element: <Login/>},
     {path: "/home", element: <Home/>},
-    {path: "/newVideo", element: <NewVideo/>}
+    {path: "/newVideo", element: <NewVideo/>},
+    {path: "/stats", element: <Stats/>}
 ])
